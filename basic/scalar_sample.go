447072d3f61d28ef3f1d09c0b950521a6b05c53c
iwa <iwacooyml@gmail.com>
2024-08-08 12:38:51 +0700

    Proto to json & json to proto

--- basic/scalar_sample.go
+++ basic/scalar_sample.go
@@ -3,17 +3,69 @@ package basic
 import (
 	"log"
 	"protobuf-go/protogen/basic"
+
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func BasicUser() {
-	scalar := basic.User{
+	u := basic.User{
 		Id:       1,
-		Username: "Ewong",
+		Username: "chilli",
 		IsActive: true,
 		Password: []byte("anjayani"),
 		Gender:   basic.Gender_GENDER_FEMALE,
-		Emails:   []string{"a@a.com"},
+		Emails:   []string{"chilli@example.com"},
 	}
-	log.Println(&scalar)
 
+	jsonBytes, _ := protojson.Marshal(&u)
+	log.Println(string(jsonBytes))
+
+}
+
+func ProtoToJson() {
+	u := basic.User{
+		Id:       1,
+		Username: "ryujin",
+		IsActive: true,
+		Password: []byte("anjayani"),
+		Gender:   basic.Gender_GENDER_FEMALE,
+		Emails:   []string{"ryujin@example.com", "makeit@example.com"},
+	}
+
+	jsonBytes, _ := protojson.Marshal(&u)
+	jsonString := string(jsonBytes)
+	log.Println(jsonString)
+
+	log.Println("=============================================")
+
+	var p basic.User
+	err := protojson.Unmarshal([]byte(jsonString), &p)
+	if err != nil {
+		log.Fatal("Got error!", err)
+	}
+
+	log.Println(&p)
+
+}
+
+func JsonToProto() {
+	jsonData := `{
+		"id": 1,
+		"username": "ryujin",
+		"is_active": true,
+		"password": "YW5qYXlhbmk=",
+		"gender": "GENDER_FEMALE",
+		"emails": [
+		  "ryujin@example.com",
+		  "makeit@example.com"
+		]
+	}`
+
+	var p basic.User
+	err := protojson.Unmarshal([]byte(jsonData), &p)
+	if err != nil {
+		log.Fatal("Got error!", err)
+	}
+
+	log.Println(&p)
 }
--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "protobuf-go/basic"
 
 func main() {
-	basic.BasicHello()
+	// basic.BasicHello()
 	basic.BasicUser()
+
+	// basic.ProtoToJson()
+	// basic.JsonToProto()
+
 }
